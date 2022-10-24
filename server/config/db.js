72e6ec2502fb973e707bc4a6d3e6348460f96532
Pak Tak Lau <plau43@my.centennialcollege.ca>
2022-10-24 02:58:55 -0400

    final

--- server/config/db.js
+++ server/config/db.js
@@ -6,4 +6,5 @@ Date: 23 Oct 2022*/
 module.exports = {
   //local MongoDB deployment ->
   "URI":"mongodb://localhost/books229",
+  "URI":"mongodb+srv://patricklmm7:Patrick54018388@cluster0.mgv8jhk.mongodb.net/books?retryWrites=true&w=majority"
 };
